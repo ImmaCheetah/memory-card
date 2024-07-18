ea82da2c7632ba6b0ee9e3c3dd12ceca2b172e80
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-07-18 14:24:51 -0400

    feat: fetch api data

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-
+  
+  useEffect(() => {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+        .then(res => res.json())
+        .then(data => console.log(data))
+  }, [])
   
   return (
     <>
