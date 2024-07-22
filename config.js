c518e77e1e1c3877d478c1b143e8866c9653c8c1
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-07-22 10:24:00 -0400

    Untrack files in .gitignore

--- config.js
+++
@@ -1,7 +0,0 @@
-export default function apiKey() {
-    return 'live_F8shDaeIQ22M7Pdeg5C4lRawfLTwbc9iPMA5xY5Tisd6C830T77lGHDeBKDr3gLR'
-}
-
-// const apiKey = {
-//     API_KEY : 'live_F8shDaeIQ22M7Pdeg5C4lRawfLTwbc9iPMA5xY5Tisd6C830T77lGHDeBKDr3gLR',
-//   }
